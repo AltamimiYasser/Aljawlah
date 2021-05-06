679bb4f27242a5b760c0a59ee396edf7ef09b06d
yasser tamimi <altamimiy89@gmail.com>
2021-05-06 15:43:52 +0300

    added icons to table

--- client/src/pages/BikesList.js
+++ client/src/pages/BikesList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState, forwardRef } from 'react';
 import ThemeContext from '../context/themeContext';
 import axios from 'axios';
 import MaterielTable from 'material-table';
@@ -7,6 +7,24 @@ import { useConfirm } from 'material-ui-confirm';
 import { makeStyles } from '@material-ui/core';
 import DeleteIcon from '@material-ui/icons/Delete';
 import notify from '../utils/notifications';
+import moment from 'moment';
+import { colors } from '../utils/styles';
+
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -43,7 +61,13 @@ const BikesList = () => {
     { title: 'Color', field: 'color' },
     { title: 'Barcode', field: 'barcode' },
     { title: 'Bill Number', field: 'billNumber' },
-    { title: 'Date of Purchase', field: 'dateOfPurchase' },
+    {
+      title: 'Date of Purchase',
+      field: 'dateOfPurchase',
+      render: (rowData) => (
+        <div>{moment(rowData.dateOfPurchase).format('DD-MM-YYYY')}</div>
+      ),
+    },
     { title: 'Model', field: 'model' },
     { title: 'Plate', field: 'plate' },
     { title: 'Rent Price', field: 'rentPrice' },
@@ -51,6 +75,33 @@ const BikesList = () => {
     { title: 'Wheels', field: 'wheels' },
     { title: 'Working Hours', field: 'workingHours' },
   ];
+
+  const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => (
+      <ChevronRight {...props} ref={ref} />
+    )),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => (
+      <ChevronLeft {...props} ref={ref} />
+    )),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => (
+      <ArrowDownward {...props} ref={ref} />
+    )),
+    ThirdStateCheck: forwardRef((props, ref) => (
+      <Remove {...props} ref={ref} />
+    )),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />),
+  };
 
   // redirect to edit bike page
   const editBike = (e, data) => {
@@ -100,17 +151,26 @@ const BikesList = () => {
         data={bikes}
         actions={[
           {
-            icon: 'edit',
+            icon: Edit,
             tooltip: 'Edit',
             onClick: editBike,
           },
           {
-            icon: 'delete',
+            icon: DeleteIcon,
             tooltip: 'Delete',
             onClick: deleteBike,
           },
         ]}
         title='Bikes List'
+        options={{
+          actionsColumnIndex: -1,
+          headerStyle: {
+            backgroundColor: colors.PRIMARY.bg,
+            color: '#fff',
+            fontSize: '1rem',
+          },
+        }}
+        icons={tableIcons}
       />
     </>
   );
